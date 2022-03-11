29add9bf7c2496da8493b99ae8f864a9731bc635
Leonel Dufau <leonelbotellero@gmail.com>
2022-03-11 00:26:16 -0300

    ejercicios 05 completos

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -222,13 +222,13 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  var array = [0]
-  var suma = numero
+  var array =[];
+  var suma = numero;
 
   for(var i = 0; i < 10 ; i++){
-    suma= suma + 2
+    suma = suma + 2
 
-    if( suma === i){
+    if(suma === i){
       break;
     } else {
       array.push(suma)
@@ -237,9 +237,8 @@ function breakStatement(numero) {
 
   if( i < 10){
     return "Se interrumpió la ejecución"
-  } else {
-    return array
-  }
+    } 
+  return array;
 }
 
 
